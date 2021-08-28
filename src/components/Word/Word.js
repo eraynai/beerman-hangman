e96374f40aeeed2bd7950f1d0e73b69ffcae4fe3
Elli Raynai <elli@elliraynai.com>
2021-08-28 11:56:19 -0400

    added word and wrong letters component to game page

---
+++ src/components/Word/Word.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Word = () => {
+	return <div className='word' id='word'></div>;
+};
+
+export default Word;
--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
 import Header from '../../components/Header/Header';
+import Figure from '../../components/Figure/Figure';
+import WrongLetters from '../../components/WrongLetters/WrongLetters';
+import Word from '../../components/Word/Word';
 
 const GamePage = () => {
 	return (
-		<div>
+		<>
 			<Header />
-		</div>
+			<div className='game-container'>
+				<Figure />
+				<WrongLetters />
+				<Word />
+			</div>
+		</>
 	);
 };
 
